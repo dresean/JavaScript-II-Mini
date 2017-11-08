a27f41106ccd77d96aa8b1a27225e41d844ed7a9
Dresean  Williams <dreseanwilliams@Dreseans-MacBook-Pro.local>
2017-11-08 18:58:58 -0500

    completed classes assignment

--- classes.js
+++ classes.js
@@ -3,7 +3,20 @@
 // problem #1
 // convert the Animal constructor function from 'constructors.js' into an ES6 class
 
+class Animal {
+  constructor (options) {
+  this.name = options.name;
+  Animal.prototype.grow = function(name) {
+    console.log(`${this.name} grew larger!`);
+  }
+ }
+}
 
+class Cat extends Animal {
+  constructor (options) {
+  super (options)
+  }
+}
 // problem #2
 // convert the Cat constructor function from 'constructors.js' into an ES6 class
 
@@ -11,9 +24,8 @@
 // if everything is setup properly the code below will print 'Foofie grew larger!'
 // uncomment the code below to test your solution
 
-// const foofie = new Cat({
-//   name: 'foofie',
-// });
-//
-// foofie.grow();
+ const foofie = new Cat({
+   name: 'foofie',
+});
+ foofie.grow();
 
